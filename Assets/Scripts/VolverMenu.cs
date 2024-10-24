0d7e832ffc77ac15bef37d300dbc165ae84f774a
luis <luis_alexissanchez@ceroca.cat>
2024-10-24 17:38:13 +0200

    Creación Mapas y Opción volver Menu

---
+++ Assets/Scripts/VolverMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class VolverMenu : MonoBehaviour
+{
+   public void volverMenu()
+    {
+        SceneManager.LoadScene("ScenaMenu");
+    }
+}
