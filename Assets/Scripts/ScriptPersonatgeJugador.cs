4f31270c2622c52d4c3a8124f5dd4c006c3fb7a9
antoni <antoni_pereblasco@ceroca.cat>
2024-11-07 19:25:35 +0100

    Principi de la funcio de moviment del personatge

---
+++ Assets/Scripts/ScriptPersonatgeJugador.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ScriptPersonatgeJugador : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private float _vel = 8;
+    void Start()   
+    {
+        _rigidbody2D =GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputHoritzontal = Input.GetAxisRaw("Horizontal") * _vel;
+
+        _rigidbody2D.velocity = new Vector2(inputHoritzontal, _rigidbody2D.velocity.y);
+
+    }
+}
