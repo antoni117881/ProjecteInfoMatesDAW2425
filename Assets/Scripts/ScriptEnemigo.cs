686291c11aa8b3420b06b6e03e45bf249f90b520
uarjona <unaxarjona@ceroca.cat>
2024-11-11 19:41:34 +0100

    Creacion de Sprites Enemigos y Movimiento

---
+++ Assets/Scripts/ScriptEnemigo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptEnemigo : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    private float _vel = 8;
+    void Start()   
+    {
+        _rigidbody2D =GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputHoritzontal = Input.GetAxisRaw("Horizontal") * _vel;
+
+        _rigidbody2D.velocity = new Vector2(inputHoritzontal, _rigidbody2D.velocity.y);
+
+        float inputVertical = Input.GetAxisRaw("Vertical") * _vel;
+        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, inputVertical);
+
+    }
+}
