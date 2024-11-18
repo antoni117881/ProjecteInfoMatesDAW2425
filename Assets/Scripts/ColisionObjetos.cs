8c58888ef21d96b50244bdc15350ece97f139a82
luis <luis_alexissanchez@ceroca.cat>
2024-11-18 20:27:25 +0100

    Objeto para enviar Mapa Uno al Mapa Dos

--- Assets/Scripts/ColisionObjetos.cs
+++ Assets/Scripts/ColisionObjetos.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEngine.SceneManagement;
+
 public class ColisionObjetos : MonoBehaviour
 {
+
+    private int siguentePantalla;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        siguentePantalla = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        SceneManager.LoadScene(siguentePantalla);
+
     }
 }
