ae38524bd5c9fe0acc328a408ad5b423e730fbe4
luis <luis_alexissanchez@ceroca.cat>
2024-11-25 19:15:13 +0100

    Modificacion Movimiento Enemig

--- Assets/Scripts/ColisionObjetos.cs
+++ Assets/Scripts/ColisionObjetos.cs
@@ -9,15 +9,34 @@ public class ColisionObjetos : MonoBehaviour
 
     private int siguentePantalla;
 
-    // Start is called before the first frame update
+
+    //[SerializeField] private string escenaCombate = "ScenaMuerte";
+    // Start 
     void Start()
     {
+        //recibe el index de cada mapa("id") +1 para que nos mande al siguente mapa dependiendo de su id 
         siguentePantalla = SceneManager.GetActiveScene().buildIndex + 1;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(siguentePantalla);
+        if (collision.CompareTag("Enemy")) //si colisiona con pj nos lleva a MapaDeCombate
+        {
+           SceneManager.LoadScene("ScenaMuerte");
+        }//else if (collision.CompareTag("Enemy")){
+        //    Debug.Log("Colisión con un enemigo.");
+        //}
+        else //nos manda al siguiente mapa
+        {
+           SceneManager.LoadScene(siguentePantalla);
+        }
+
+        
 
     }
+      
+    
+    
+
+
 }
--- Assets/Scripts/ScriptEnemigo.cs
+++ Assets/Scripts/ScriptEnemigo.cs
@@ -7,11 +7,40 @@ public class ScriptEnemigo : MonoBehaviour
     private float _vel = 8;
     private Camera _camara;
 
+    Vector2 pantallaMin;
+    Vector2 pantallaMax;
+    Vector2 direccionAleatoria;
+
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _camara = Camera.main; // Obtener la cámara principal
-        StartCoroutine(MoverAleatoriamente());
+        //_camara = Camera.main; // Obtener la cámara principal
+        //StartCoroutine(MoverAleatoriamente());
+
+        pantallaMin = new Vector2(-7.3f, -3.3f);
+        pantallaMax = new Vector2(5.3f, 3f);
+
+        InvokeRepeating("MovimentAleatori", 0f, 1f);
+    }
+
+    private void Update()
+    {
+        Vector2 novaPos = transform.position;
+        novaPos = novaPos + _vel * direccionAleatoria * Time.deltaTime;
+
+        novaPos.x = Mathf.Clamp(novaPos.x, pantallaMin.x, pantallaMax.x);
+        novaPos.y = Mathf.Clamp(novaPos.y, pantallaMin.y, pantallaMax.y);
+
+        transform.position = novaPos;
+    }
+
+    void MovimentAleatori()
+    {
+        direccionAleatoria = new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f)).normalized;
+
+        //_rigidbody2D.velocity = direccionAleatoria * _vel;
+
+        //LimitarPosicionDentroDeCamara();
     }
 
     IEnumerator MoverAleatoriamente()
@@ -33,11 +62,13 @@ public class ScriptEnemigo : MonoBehaviour
 
     void LimitarPosicionDentroDeCamara()
     {
-  
-        Vector2 pantallaMin = _camara.ViewportToWorldPoint(new Vector2(0, 0)); 
-        Vector2 pantallaMax = _camara.ViewportToWorldPoint(new Vector2(1, 1)); 
 
-    
+        //Vector2 pantallaMin = _camara.ViewportToWorldPoint(new Vector2(0, 0)); 
+        //Vector2 pantallaMax = _camara.ViewportToWorldPoint(new Vector2(1, 1)); 
+        //Vector2 pantallaMin = new Vector2(-7.3f, -3.3f);
+        //Vector2 pantallaMax = new Vector2(5.3f, 3f);
+
+
         float x = Mathf.Clamp(transform.position.x, pantallaMin.x, pantallaMax.x);
         float y = Mathf.Clamp(transform.position.y, pantallaMin.y, pantallaMax.y);
 
