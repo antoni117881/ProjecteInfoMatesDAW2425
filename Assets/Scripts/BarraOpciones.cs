8973c097aa2e37b17a1cbc92fe074b2f0243b3f2
luis <luis_alexissanchez@ceroca.cat>
2024-12-01 16:36:09 +0100

    Solución de ERRORES

--- Assets/Scripts/BarraOpciones.cs
+++ Assets/Scripts/BarraOpciones.cs
@@ -11,9 +11,13 @@ public class BarraOpciones : MonoBehaviour
 {
 
 
+    
     // _______________  BOTON PAUSA_______________ // 
     private bool juegoPausado = false;
-    public  TextMeshProUGUI  textoPausa; // Referencia al texto UI que mostrará el mensaje
+    public TextMeshProUGUI textoPausa; // Referencia al texto UI que mostrará el mensaje
+
+    // Panel de confirmación de salida
+    public GameObject panelConfirmacionSalir; // Panel con los botones "Sí" y "No"
 
     // Método para pausar el juego y mostrar texto
     public void PausarJuego()
@@ -31,6 +35,27 @@ public class BarraOpciones : MonoBehaviour
             textoPausa.text = "El juego se ha pausado"; // Actualizar el mensaje
             textoPausa.gameObject.SetActive(true); // Mostrar el texto
         }
+    }
+
+    // Mostrar el panel de confirmación cuando se intenta salir del juego
+    public void MostrarPanelConfirmacion()
+    {
+        panelConfirmacionSalir.SetActive(true); // Activar el panel de confirmación
+        Time.timeScale = 0f; // Pausar el juego al mostrar el panel
+    }
+
+    // Si el jugador hace clic en "Sí", volver al menú principal
+    public void VolverAlMenu()
+    {
+        PlayerPrefs.SetString("UltimaEscena", SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("ScenaMenu");
+    }
+
+    // Si el jugador hace clic en "No", cerrar el panel y continuar con el juego
+    public void CancelarSalida()
+    {
+        panelConfirmacionSalir.SetActive(false); // Desactivar el panel de confirmación
+        Time.timeScale = 1f; // Reanudar el juego
     }
 }
     
